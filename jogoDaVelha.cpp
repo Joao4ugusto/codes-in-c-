b6a3c53cd60fcacda1c2aeb7eef73bdf0adc6650
João <joaoaugusto311016@gmail.com>
2023-04-20 00:52:18 -0300

    20-04-23

--- jogoDaVelha.cpp
+++ jogoDaVelha.cpp
@@ -10,7 +10,13 @@ int main(){
     cout << "Numero secreto eh: " << num_secreto << endl;
 
     int chute;
-    cout << "Qual seu chute:" << endl;
+    cout << "Qual seu chute? ";
     cin >> chute;
     cout << "O numero do chute foi " << chute << endl;
+
+    if(chute == num_secreto){
+        cout << "Prabens vc acertou!!";
+    } else {
+        cout << "Esse nao eh o numero!!";
+    }
 }
