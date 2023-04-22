63e933f13779eea45894956c37466393c0f9cf0c
João <joaoaugusto311016@gmail.com>
2023-04-22 20:40:25 -0300

    22-04

--- Forca.cpp
+++ Forca.cpp
@@ -20,11 +20,11 @@ int main(){
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
-    
-    int tentativas = 0; // número de tentativas feitas pelo jogador
-    
-    while(nao_acertou && nao_enforcou && tentativas < 7){ // adicionado o limite de tentativas
-        
+
+    // int tentativas = 0; // número de tentativas feitas pelo jogador
+
+    while(nao_acertou && nao_enforcou){ // adicionado o limite de tentativas
+
         for(int i = 0; i < PALAVRA_SECRETA.size(); i++){
             if(chutou[PALAVRA_SECRETA[i]]){
                 cout << PALAVRA_SECRETA[i] << " ";
@@ -32,24 +32,24 @@ int main(){
                 cout << "_ ";
             }
         }
-        
+
         cout << endl;
         char chute;
         cin >> chute;
-        
+
         chutou[chute] = true;
-        tentativas++; // incrementa o número de tentativas
-        
+        // tentativas++; // incrementa o número de tentativas
+
         cout << "seu chute foi " << chute << endl;
-        
+
         if(letraExiste(chute)){
             cout << "Voce acertou!! Seu chute esta na palavra!!" << endl;
         } else {
             cout << "Voce errou!! Seu chute nao esta na palavra!!" << endl;
         }
-        
+
     }
-    
+
     system("pause"); // pausa a execução para visualizar o resultado
     return 0; // indica que o programa terminou com sucesso
 }
