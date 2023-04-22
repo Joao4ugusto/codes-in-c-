af5d7324ff2c50d65e77c8b5533abc75c48e898c
João <joaoaugusto311016@gmail.com>
2023-04-22 20:30:22 -0300

    22-04-23

--- Forca.cpp
+++ Forca.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <cstdlib> // para usar system("pause")
 using namespace std;
 
- string PALAVRA_SECRETA = "MELANCIA";
+string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
- bool letraExiste(const char chute){
+bool letraExiste(const char chute){
     for(int i = 0; i < PALAVRA_SECRETA.size(); i++){
         if(chute == PALAVRA_SECRETA[i]){
             return true;
         }
     }
     return false;
- }
+}
 
 int main(){
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
-
-    while(nao_acertou && nao_enforcou){
-       char chute;
-       cout << "Digite seu chute ";
-       cin >> chute;
-
-       cout << "seu chute foi " << chute << endl;
-
-       if(letraExiste(chute)){
-        cout << "Voce acertou!! Seu chute esta na palavra!!" << endl;
-       } else {
-        cout << "Voce errou!! Seu chute nao esta na palavra!!" << endl;
-       }
+    
+    int tentativas = 0; // número de tentativas feitas pelo jogador
+    
+    while(nao_acertou && nao_enforcou && tentativas < 7){ // adicionado o limite de tentativas
+        
+        for(int i = 0; i < PALAVRA_SECRETA.size(); i++){
+            if(chutou[PALAVRA_SECRETA[i]]){
+                cout << PALAVRA_SECRETA[i] << " ";
+            } else {
+                cout << "_ ";
+            }
+        }
+        
+        cout << endl;
+        char chute;
+        cin >> chute;
+        
+        chutou[chute] = true;
+        tentativas++; // incrementa o número de tentativas
+        
+        cout << "seu chute foi " << chute << endl;
+        
+        if(letraExiste(chute)){
+            cout << "Voce acertou!! Seu chute esta na palavra!!" << endl;
+        } else {
+            cout << "Voce errou!! Seu chute nao esta na palavra!!" << endl;
+        }
+        
     }
+    
+    system("pause"); // pausa a execução para visualizar o resultado
+    return 0; // indica que o programa terminou com sucesso
 }
